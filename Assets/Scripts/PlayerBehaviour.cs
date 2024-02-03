8524f0fd36e88d29b138284e1275fdf092c48c95
Alexey_Dzhevello <fishwere@mail.ru>
2024-02-03 05:03:35 +0500

    fixed height-inconsistent-jump issue

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,7 +9,6 @@ public class PlayerBehaviour : MonoBehaviour
     private float FallSpeedBlocksPerSecond = 20;
 
     private float JumpSpeedBlocksPerSecond = 20;
-
     private float JumpHeightBlocks = 6;
 
     private float DashSpeedBlocksPerSecond = 50;
@@ -20,11 +19,6 @@ public class PlayerBehaviour : MonoBehaviour
     private Vector2 desiredMoveDirection;
 
     private Vector2 lastNonzeroDesiredMoveDirection = Vector2.right;
-
-    private Vector2 dashDirection = Vector2.right;
-
-    private Vector2 lastPlayerPosition = Vector2.zero;
-    private int theSameCount = 0;
 
     private Rigidbody2D rb;
 
@@ -54,7 +48,7 @@ public class PlayerBehaviour : MonoBehaviour
         rb.velocity = Vector2.zero;
         if (state == PlayerState.Regular)
         {
-            PerformWalk();
+            PerformMove();
             PerformFall();
         }
         else if(state == PlayerState.Dashing)
@@ -64,34 +58,18 @@ public class PlayerBehaviour : MonoBehaviour
         else if (state == PlayerState.Jumping)
         {
             PerformJump();
-            PerformWalk();
+            PerformMove();
             StopJumpFromCollision();
         }
         else if (state == PlayerState.NotFalling)
         {
-            PerformWalk();
+            PerformMove();
         }
-        lastPlayerPosition = transform.position;
-    }
-
-    private void PerformWalk()
-    {
-        rb.velocity += (new Vector2(desiredMoveDirection[0], 0).normalized) * WalkSpeedBlocksPerSecond;
     }
 
     private void PerformFall()
     {
         rb.velocity -= new Vector2(0, 1) * FallSpeedBlocksPerSecond;
-    }
-
-    private void PerformJump()
-    {
-        rb.velocity += new Vector2(0, 1) * JumpSpeedBlocksPerSecond;
-    }
-
-    private void PerformDash()
-    {
-        rb.velocity += dashDirection * DashSpeedBlocksPerSecond;
     }
 
     private bool IsGrounded()
@@ -103,14 +81,77 @@ public class PlayerBehaviour : MonoBehaviour
         return Physics2D.OverlapArea(p1, p2, GroundLayerMask);
     }
 
+    private void SetState(PlayerState nextState)
+    {
+        if (state == PlayerState.Jumping)
+        {
+            OnJumpTermination();
+        }
+        else if (state == PlayerState.Dashing)
+        {
+            OnDashTermination();
+        }
+        else if (state == PlayerState.NotFalling)
+        {
+            OnNotFallingTermination();
+        }
+
+
+        if (nextState == PlayerState.Jumping)
+        {
+            OnJumpSetup();
+        }
+        else if (nextState == PlayerState.Dashing)
+        {
+            OnDashSetup();
+        }
+        else if (nextState == PlayerState.NotFalling)
+        {
+            OnNotFallingSetup();
+        }
+
+        state = nextState;
+    }
+
+    #region Jump
+
+    private float beforeJumpHeight;
+
+    private float lastAbsoluteHeight;
+
+    private float timeSinceHeightNotChanging;
+
+    private void OnJumpSetup()
+    {
+        beforeJumpHeight = transform.position.y;
+        timeSinceHeightNotChanging = 0;
+        StartCoroutine(StopJumpFromTimeout());
+    }
+
+    private void OnJumpTermination()
+    {
+        //do what needs to be done when jump is terminated
+    }
+
+    //public event
     public void OnJumpChanged(bool start)
     {
         Debug.Log("jump you dog!");
         if (start && state == PlayerState.Regular && IsGrounded())
         {
-            state = PlayerState.Jumping;
-            StartCoroutine(StopJumpFromTimeout());
+            SetState(PlayerState.Jumping);
         }
+    }
+
+    private void PerformJump()
+    {
+        var deltaHeight = Mathf.Abs(beforeJumpHeight - transform.position.y);
+        if (deltaHeight > JumpHeightBlocks - 1e-3)
+        {
+            return;
+        }
+
+        rb.velocity += new Vector2(0, 1) * JumpSpeedBlocksPerSecond;
     }
 
     private IEnumerator StopJumpFromTimeout()
@@ -118,7 +159,7 @@ public class PlayerBehaviour : MonoBehaviour
         yield return new WaitForSeconds(JumpHeightBlocks / JumpSpeedBlocksPerSecond);
         if (state == PlayerState.Jumping)
         {
-            state = PlayerState.NotFalling;
+            SetState(PlayerState.NotFalling);
             StartCoroutine(ResumeFallingFromTimeout());
         }
     }
@@ -126,22 +167,26 @@ public class PlayerBehaviour : MonoBehaviour
     private void StopJumpFromCollision()
     {
         
-        if (state == PlayerState.Jumping && Mathf.Abs(lastPlayerPosition.y - transform.position.y) < 1e-2)
+        if (state == PlayerState.Jumping && Mathf.Abs(lastAbsoluteHeight - transform.position.y) < 1e-2)
         {
-            Debug.Log(theSameCount);
-            //use time instead
-            theSameCount++;
-            if (theSameCount > 10)
+            Debug.Log(timeSinceHeightNotChanging);
+            timeSinceHeightNotChanging += Time.deltaTime;
+            if (timeSinceHeightNotChanging > 0.1f)
             {
-                state = PlayerState.Regular;
-                theSameCount = 0;
+                //not a very good move though
+                SetState(PlayerState.Regular);
             }
         }
         else
         {
-            theSameCount = 0;
+            timeSinceHeightNotChanging = 0;
         }
+        lastAbsoluteHeight = transform.position.y;
     }
+
+    #endregion
+
+    #region Move
 
     public void OnMoveChanged(Vector2 direction)
     {
@@ -152,15 +197,52 @@ public class PlayerBehaviour : MonoBehaviour
         }
     }
 
+    public void PerformMove()
+    {
+        rb.velocity += (new Vector2(desiredMoveDirection[0], 0).normalized) * WalkSpeedBlocksPerSecond;
+    }
+
+    #endregion
+
+    #region Dash
+
+    private Vector2 dashDirection = Vector2.right;
+
+    private Vector2 beforeDashPosition;
+
+    private void OnDashSetup()
+    {
+        /*passedDashTime = 0;
+        requiredDashTime = DashDistanceBlocks / DashSpeedBlocksPerSecond;*/
+        beforeDashPosition = transform.position;
+        StartCoroutine(StopDashFromTimeout());
+    }
+
+    private void OnDashTermination()
+    {
+
+    }
+
     public void OnDashChanged()
     {
         Debug.Log("Dash it");
         dashDirection = lastNonzeroDesiredMoveDirection;
         if (state == PlayerState.Regular || state == PlayerState.Jumping || state == PlayerState.NotFalling)
         {
-            state = PlayerState.Dashing;
-            StartCoroutine(StopDashFromTimeout());
+            SetState(PlayerState.Dashing);
         }
+    }
+
+    private void PerformDash()
+    {
+        var delta = ((Vector2)transform.position - beforeDashPosition).magnitude;
+
+        if (delta > DashDistanceBlocks - 1e-2)
+        {
+            return;
+        }
+
+        rb.velocity += dashDirection * DashSpeedBlocksPerSecond;
     }
 
     public IEnumerator StopDashFromTimeout()
@@ -168,9 +250,22 @@ public class PlayerBehaviour : MonoBehaviour
         yield return new WaitForSeconds(DashDistanceBlocks / DashSpeedBlocksPerSecond);
         if (state == PlayerState.Dashing)
         {
-            state = PlayerState.NotFalling;
-            StartCoroutine(ResumeFallingFromTimeout());
+            SetState(PlayerState.NotFalling);
         }
+    }
+
+    #endregion
+
+    #region NotFalling
+
+    private void OnNotFallingSetup()
+    {
+        StartCoroutine(ResumeFallingFromTimeout());
+    }
+
+    private void OnNotFallingTermination()
+    {
+        Debug.Log("terminate not falling");
     }
 
     private IEnumerator ResumeFallingFromTimeout()
@@ -178,9 +273,11 @@ public class PlayerBehaviour : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         if (state == PlayerState.NotFalling)
         {
-            state = PlayerState.Regular;
+            SetState(PlayerState.Regular);
         }
     }
+
+    #endregion
 }
 
 
